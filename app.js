5256ba1562a746213d02d77f7c7b8afd5579737d
jababros <hahahayamhaihum@gmail.com>
2020-05-18 23:59:16 +0530

    second commit

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ app.use(express.static("public"));
 app.set('view engine','ejs');
 const books = ["pices","harry potter","header"];
 ////////////////////////////////////////mongoose\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-mongoose.connect('mongodb://localhost:27017/pusatakDB', {useNewUrlParser: true,useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://anubhavjaba:Asaurabh@99@jaba-aolqk.mongodb.net/test?retryWrites=true&w=majority/pustakDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 const loginSchema = new mongoose.Schema(
   { name:String,
     email:String,
@@ -27,18 +30,20 @@ app.route("/")
 /////////////\\\\\\\\\\\sigin route\\\\\\\\\\\\\\\//////////////
 app.route("/signin")
 .get(function(req,res){
-  res.render("signin");
+  res.render("signin",{message:"",message2:""});
 })
 .post(function(req,res){
   let email = req.body.email;
   let password= req.body.password;
   loginData.find({email:email,password:password},function(err,result){
+    console.log(result)
     if (result.email==email) {
       res.redirect("/")
     }
     else {
-      res.
-      Alert("no account exists");
+      res.render("signin",{
+        message:"no account found",message2:""
+      })
     }
   });
 });
@@ -50,8 +55,8 @@ app.route("/signup")
     let password  = req.body.password;
     let genere = req.body.genere;
     loginData.find({email:email},function(err,result){
-      if (result) {
-      res.render()
+      if (result.email==email) {
+      res.render("signin",{message:"",message2 :"sorry the account already exists"})
       }
       else{
         var data = new loginData({
@@ -61,7 +66,14 @@ app.route("/signup")
            genere:genere
          });
          data.save();
+         res.redirect("/");
       }
+    })
+  });
+  app.get("/data",function(req,res)
+  {
+    loginData.find({},function(err,res){
+      console.log(res);
     })
   });
 app.listen("3000",function(req,res){
